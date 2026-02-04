52b274be6cdee2f2e2e4a198860c6b4f74f0e90e
Tuhin <tuhin@tuhin.com>
2026-02-04 19:44:47 +0530

    fix firebase json

--- database/firebaseAdmin.js
+++ database/firebaseAdmin.js
@@ -1,8 +1,21 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-key.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // 🚀 Railway / Production
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    console.log("🔥 Firebase loaded from ENV");
+} else {
+    // 💻 Local development
+    serviceAccount = require("./firebase-key.json");
+    console.log("💻 Firebase loaded from local file");
+}
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
+module.exports = admin;
 
 const db = admin.firestore();
 
